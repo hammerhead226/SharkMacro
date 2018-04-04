79a519a59939444e55b9b9569ff8f418b9b68d7d
vrrajesh21 <varun.rajesh12@hotmail.com>
2018-04-03 20:33:51 -0400

    added shark math
    This holds all the math needed to calculate the next position of each point

--- src/org/hammerhead226/sharkmacro/display/SharkMath.java
+++ src/org/hammerhead226/sharkmacro/display/SharkMath.java
@@ -4,21 +4,57 @@ import java.awt.Point;
 
 public class SharkMath {
 	
-	public static double findRadius(double w, double p_i, double p_o) {
-		return (w * p_i) / (p_o - p_i);
+	double w, p_i, p_o, r, theta, imag, omag, heading;
+	Point i, o, center;
+	
+	
+	public SharkMath(double w, double p_i, double p_o, Point i, Point o) {
+		this.w = w;
+		this.p_i = p_i;
+		this.p_o = p_o;
+		
+		this.i = i;
+		this.o = o;
+		
+		findRadius();
+		findAngle();
+		findInsideMag();
+		findOutsideMag();
+		findHeading();
 	}
 	
-	public static double findAngle(double w, double p_i, double p_o) {
-		return (p_o - p_i) / w;
+	private void findRadius() {
+		r = (w * p_i) / (p_o - p_i);
 	}
 	
-	public static Point findCenter(Point i, Point o, double p_i, double p_o) {
-		double w = Math.sqrt(Math.pow(i.x - o.x, 2) + Math.pow(i.y - o.x, 2));
+	private void findAngle() {
+		theta = (p_o - p_i) / w;
+	}
+	
+	private void findInsideMag() {
+		imag = Math.sqrt(2 * r * r * (1 + Math.cos(theta)));
+	}
+	
+	private void findOutsideMag() {
+		omag = Math.sqrt(2 * (r+w) * (r+w) * (1 + Math.cos(theta)));
+	}
+	
+	private void findHeading() {
+		double alpha = Math.atan2(i.y - o.y, i.x - o.x);
 		
-		double r = findRadius(w, p_i, p_o);
+		heading = alpha + theta / 2 - Math.PI / 2;
+	}
+	
+	public Point getFinalPositionOfInside() {
+		double x = imag * Math.cos(heading) + i.x;
+		double y = imag * Math.sin(heading) + i.y;
 		
-		double y = ((r + w) * (i.y - o.y) + w *o.y) / w;
-		double x = ((r + w) * (i.x - o.x) + w *o.x) / w;
+		return new Point((int) x, (int) y);
+	}
+	
+	public Point getFinalPositionOfOutside() {
+		double x = omag * Math.cos(heading) + o.x;
+		double y = omag * Math.sin(heading) + o.y;
 		
 		return new Point((int) x, (int) y);
 	}
